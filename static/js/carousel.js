3a190c3c3bed42c020cae544b617bafd175baea0
Adrian Hernandez <ad24.ra12@gmail.com>
2022-12-12 00:09:09 -0600

    Finished testimonials, done with slider, still missing to fix it since, when scrolling it doesn't center the new slide coming in

---
+++ static/js/carousel.js
@@ -0,0 +1,31 @@
+let carousel = document.querySelector(".carousel");
+let carousel_items = document.getElementsByClassName("hero-card--testimonial");
+let carousel_current_item = 1;
+let last_translated = 0;
+let max_translated = 0;
+function goToNext(){
+    if(carousel_current_item < carousel_items.length){
+        last_translated = last_translated -100;
+        if(max_translated > last_translated){
+            max_translated=last_translated
+        }
+        console.log(last_translated)
+        carousel.style.transform = "translateX("+last_translated+"%)";
+        carousel_current_item++;
+    }else{
+        last_translated = 0;
+        carousel.style.transform = "translateX(0%)";
+        carousel_current_item=1;
+    }
+}
+function goToPrevious(){
+    if(carousel_current_item > 1){
+        last_translated = last_translated+100
+        carousel.style.transform = "translateX("+last_translated+"%)";
+        carousel_current_item--;
+    }else{
+        last_translated = max_translated = -100*(carousel_items.length-1)
+        carousel.style.transform = "translateX("+max_translated+"%)";
+        carousel_current_item=carousel_items.length;
+    }
+}
