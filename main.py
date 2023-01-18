8dd2ccadbaa1cf1d9306f1175884fafd4e406b60
Adrian Hernandez <ad24.ra12@gmail.com>
2023-01-18 14:11:41 -0600

    Finished responsive design for both Carousel-Card and Footer, fixed Skill-Card for responsive (in responsive the image grew larger than the container)

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ app = Flask(__name__, static_folder="./static/", template_folder="./templates/")
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+@app.route("/testimonials")
+def testimonials():
+    return render_template('testimonials.html')
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
