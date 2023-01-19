03c1694d360968f228905d63574e69ca4627dac6
Adrian Hernandez <ad24.ra12@gmail.com>
2023-01-19 11:29:36 -0600

    Finished styling Testimonials and Contact, still missing to add JS to send form submissions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__, static_folder="./static/", template_folder="./templates/")
 
@@ -15,3 +15,15 @@ def testimonials():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
+@app.route("/contact/submit", methods=["POST"])
+def contact_form_submit():
+    form_name = request.form.get("form-name")
+    form_email = request.form.get("form-email")
+    form_message = request.form.get("form-message")
+    print("\n[MENSAJES POST]")
+    print(f"[form_name]: [{form_name}]")
+    print(f"[form_email]: [{form_email}]")
+    print(f"[form_message]: [{form_message}]")
+    return 200
+
