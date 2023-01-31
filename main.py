878599fd9e33f09fd0886260aa489fc9e2e8bfd1
Adrian Hernandez <ad24.ra12@gmail.com>
2023-01-31 15:34:26 -0600

    Removed rotator animation

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template,request, make_response, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__, static_folder="./static/", template_folder="./templates/")
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'programmer'
+app.config['MYSQL_DB'] = 'comments_submissions'
+ 
+mysql = MySQL(app)
 
 def make_custom_response(status: str, message: str):
     return make_response(
@@ -33,5 +40,17 @@ def contact_form_submit():
     form_message = request.form.get("form-message-textarea")
     if(form_name == "" or form_email == "" or form_message == ""):
         return make_custom_response("400", "There're some empty fields!")
+    cursor = mysql.connection.cursor()
+    try:
+        query_result = cursor.execute(f"""INSERT INTO submissions (submission_user_name,submission_user_email,submission_user_msg)
+                       VALUES ('{form_name}','{form_email}','{form_message}')""")
+        if(query_result == 0):
+            return make_custom_response("500", "Error when submiting your message, please try again later!")
+    except Exception as e:
+        print("\n[Exception commiting to MySQL]")
+        print(e)
+        return make_custom_response("500", "Error when submiting your message, please try again later!")
+    mysql.connection.commit()
+    cursor.close()
     return make_custom_response("200","Ok") 
 
